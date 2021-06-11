bacd51bf54d6ddf2dd77d9a0a0a8d9f411147a60
Minwoo <minwoojung01@gmail.com>
2021-06-10 19:33:06 -0700

    Add PostImage

--- components/PostImages.js
+++ components/PostImages.js
@@ -1,7 +1,77 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
+import { PlusOutlined } from '@ant-design/icons';
 
-function PostImages() {
-  return <div></div>;
+function PostImages({ images }) {
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+
+  if (images.length === 2) {
+    return (
+      <>
+        <img
+          role="presentation"
+          style={{ width: '50%', display: 'inline-block' }}
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          role="presentation"
+          style={{ width: '50%', display: 'inline-block' }}
+          src={images[1].src}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <img
+        role="presentation"
+        style={{ width: '50%', display: 'inline-block' }}
+        src={images[0].src}
+        alt={images[0].src}
+        onClick={onZoom}
+      />
+      <div
+        role="presentation"
+        style={{
+          display: 'inline-block',
+          width: '50%',
+          textAlign: 'center',
+          verticalAlign: 'middle',
+        }}
+        onClick={onZoom}
+      >
+        <PlusOutlined />
+        <br />
+        {images.length - 1} See More
+      </div>
+    </>
+  );
 }
+
+PostImages.propTypes = {
+  images: PropTypes.array.isRequired,
+};
 
 export default PostImages;
