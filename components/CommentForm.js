14f7fe9caed45327bfe4994aa44190bf220ac982
Minwoo <minwoojung01@gmail.com>
2021-06-10 17:33:56 -0700

    Add CommentForm

--- components/CommentForm.js
+++ components/CommentForm.js
@@ -1,8 +1,38 @@
-import { Form } from 'antd';
-import React from 'react';
+import { Form, Input, Button } from 'antd';
+import React, { useCallback } from 'react';
+import useInput from '../hooks/useInput';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+
+const { Item } = Form;
+const { TextArea } = Input;
 
 function CommentForm({ post }) {
-  return <Form>Form</Form>;
+  const id = useSelector((state) => state.user.me?.id);
+  const [commentText, onChangeCommentText] = useInput('');
+
+  const onSubmitComment = useCallback(() => {
+    console.log(post.id, commentText);
+  }, [commentText]);
+
+  return (
+    <Form onFinish={onSubmitComment}>
+      <Item style={{ position: 'relative', margin: 0 }}>
+        <TextArea value={commentText} onChange={onChangeCommentText} rows={4} />
+        <Button
+          style={{ position: 'absolute', right: 0, bottom: -40 }}
+          type="primary"
+          htmlType="submit"
+        >
+          Retweet
+        </Button>
+      </Item>
+    </Form>
+  );
 }
+
+CommentForm.propTypes = {
+  post: PropTypes.object.isRequired,
+};
 
 export default CommentForm;
