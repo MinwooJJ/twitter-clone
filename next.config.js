994cc89cfb87a6c3c7dfb438d21b8580157445f2
Minwoo <minwoojung01@gmail.com>
2021-07-03 18:45:09 -0700

    Add config webpack

---
+++ next.config.js
@@ -0,0 +1,23 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  compress: true,
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === 'production';
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval',
+      module: {
+        ...config.module,
+        rules: [...config.module.rules, {}],
+      },
+      plugins: [
+        ...config.plugins,
+        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/en$/),
+      ],
+    };
+  },
+});
